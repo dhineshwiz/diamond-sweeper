b9bfb5101fe20b5a559e29efb78b318a874c89dc
malvia <radhikamalviya26@gmail.com>
2017-10-01 00:40:41 -0700

    generating diamonds at random positions

--- diamond-sweeper/app/client/app.js
+++ diamond-sweeper/app/client/app.js
@@ -1,15 +1,16 @@
-global.startApp = function(container) {
-    console.log("Here is the container:", container);
-    $(document).ready(function() {
-        initializeBoard(board_row, board_col);
-    });
-}
-
-
 var board_row = 8;
 var board_col = 8;
 var cellNum = 0;
 var cellCordinates = {};
+var diamond_count = 8;
+var diamondSet = {};
+global.startApp = function(container) {
+    console.log("Here is the container:", container);
+    $(document).ready(function() {
+        randomGenerators(diamond_count);
+        initializeBoard(board_row, board_col);
+    });
+}
 
 function initializeBoard(board_row, board_col) {
     var table = document.createElement("table");
@@ -30,4 +31,11 @@ function initializeBoard(board_row, board_col) {
         table.appendChild(tr);
     }
     document.getElementById("game").appendChild(table);
+}
+
+function randomGenerators(n) {
+    while (Object.keys(diamondSet).length < n) {
+        var randomnumber = Math.ceil(Math.random() * 63)
+        diamondSet[randomnumber] = randomnumber;
+    }
 }
